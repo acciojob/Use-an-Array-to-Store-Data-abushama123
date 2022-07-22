9cd092ef8ce930490c43b75aa41412cf0ee97713
Abushama Siddique <abushamasiddique175@gmail.com>
2022-07-22 19:04:25 +0530

    Assignment Done

--- solution.js
+++ solution.js
@@ -3,5 +3,6 @@ let yourArray;
 // assign your array the correct value in the next line
 // your code here!
 // code end
+yourArray = ["Acciojob", " Abushama",true,1,5];
 
 module.exports = yourArray
